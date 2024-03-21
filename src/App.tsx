e4286380ce98af236aa7e550e7f8a29f1d926aac
Andrew <andrewlokoffical@gmail.com>
2024-03-21 19:41:00 +0100

    Add Pizza Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
+import AddPizzaForm from './components/AddPizzaForm';
 import './App.css';
 
-type Order = {
-  title: string,
-  quantity?: number
-}
-
-const order: Order = {title: 'margarita', quantity: 10}
-
-const orders: Order[] = [
-  {title: 'margarita'},
-  {title: 'salami', quantity: 10}
-];
-
-let number: null | number = null
-
-number = 10
-
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
-      New App!
+     <div className="wrap">
+        <span className='heading'>Our Pizzeria</span>
+        <AddPizzaForm />
+     </div>
     </div>
   );
 }
